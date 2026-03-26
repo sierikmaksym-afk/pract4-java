41ad4f72ddb8ace852a4ea576ac219ee9e958a8d
sierikmaksym-afk <sierik.maksym@student.sumdu.edu.ua>
2026-03-26 18:45:58 +0200

    configure DB

---
+++ create_schema.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE employees (
+   id INTEGER PRIMARY KEY,
+   type VARCHAR(50) NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   salary DOUBLE PRECISION NOT NULL,
+   contract_months INTEGER,
+   bonus DOUBLE PRECISION,
+   work_country VARCHAR(255),
+   team_size INTEGER
+);
