f4c3a577e4be06bf037b089dd1777b3922fd2167
sierikmaksym-afk <sierik.maksym@student.sumdu.edu.ua>
2026-03-26 17:08:45 +0200

    Add Unit tests

---
+++ src/test/java/EmployeeTests.java
@@ -0,0 +1,25 @@
+﻿import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Тести для класу Employee.
+ */
+class EmployeeTest {
+
+    @Test
+    void shouldThrowExceptionWhenInvalidValueInSetter() {
+        Employee obj = new Employee(1, "Employee", 1000.0);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            obj.setSalary(-1);
+        });
+    }
+
+    @Test
+    void shouldThrowExceptionWhenInvalidConstructorData() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Employee(0, "", -5);
+        });
+    }
+}
