5e1e954dfac7d61f7e1d39eebd36edfb881ab9cf
sierikmaksym-afk <sierik.maksym@student.sumdu.edu.ua>
2026-03-26 17:25:32 +0200

    Add ContractEmployee

---
+++ src/main/java/ContractEmployee.java
@@ -0,0 +1,44 @@
+﻿/**
+ * Похідний клас, що описує контрактного працівника.
+ */
+public class ContractEmployee extends Employee {
+    private int contractMonths;
+
+    /**
+     * Конструктор для створення контрактного працівника.
+     */
+    public ContractEmployee(int id, String name, double salary, int contractMonths) {
+        super(id, name, salary);
+        setContractMonths(contractMonths);
+    }
+
+    /**
+     * Повертає тривалість контракту в місяцях.
+     */
+    public int getContractMonths() {
+        return contractMonths;
+    }
+
+    /**
+     * Встановлює тривалість контракту в місяцях.
+     */
+    public void setContractMonths(int contractMonths) {
+        if (contractMonths <= 0) {
+            throw new IllegalArgumentException("Тривалість контракту повинна бути більше 0.");
+        }
+        this.contractMonths = contractMonths;
+    }
+
+    /**
+     * Повертає рядкове представлення об'єкта.
+     */
+    @Override
+    public String toString() {
+        return "ContractEmployee{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", salary=" + getSalary() +
+                ", contractMonths=" + contractMonths +
+                '}';
+    }
+}
