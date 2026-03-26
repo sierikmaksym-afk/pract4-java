26356bfc3f6464bb08598a0c9634beb3519ed693
sierikmaksym-afk <sierik.maksym@student.sumdu.edu.ua>
2026-03-26 17:25:41 +0200

    Add FullTimeEmployee

---
+++ src/main/java/FullTimeEmployee.java
@@ -0,0 +1,44 @@
+﻿/**
+ * Похідний клас, що описує штатного працівника.
+ */
+public class FullTimeEmployee extends Employee {
+    private double bonus;
+
+    /**
+     * Конструктор для створення штатного працівника.
+     */
+    public FullTimeEmployee(int id, String name, double salary, double bonus) {
+        super(id, name, salary);
+        setBonus(bonus);
+    }
+
+    /**
+     * Повертає бонус працівника.
+     */
+    public double getBonus() {
+        return bonus;
+    }
+
+    /**
+     * Встановлює бонус працівника.
+     */
+    public void setBonus(double bonus) {
+        if (bonus < 0) {
+            throw new IllegalArgumentException("Бонус не може бути від'ємним.");
+        }
+        this.bonus = bonus;
+    }
+
+    /**
+     * Повертає рядкове представлення об'єкта.
+     */
+    @Override
+    public String toString() {
+        return "FullTimeEmployee{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", salary=" + getSalary() +
+                ", bonus=" + bonus +
+                '}';
+    }
+}
