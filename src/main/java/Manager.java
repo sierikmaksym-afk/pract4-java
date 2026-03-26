5f2bbe117d2b88c8a212d5a1beeb9e1eefba96d9
sierikmaksym-afk <sierik.maksym@student.sumdu.edu.ua>
2026-03-26 17:34:32 +0200

    Add Manager

---
+++ src/main/java/Manager.java
@@ -0,0 +1,44 @@
+﻿/**
+ * Клас, що описує менеджера.
+ */
+public class Manager extends Employee {
+    private int teamSize;
+
+    /**
+     * Конструктор для створення менеджера.
+     */
+    public Manager(int id, String name, double salary, int teamSize) {
+        super(id, name, salary);
+        setTeamSize(teamSize);
+    }
+
+    /**
+     * Повертає кількість підлеглих.
+     */
+    public int getTeamSize() {
+        return teamSize;
+    }
+
+    /**
+     * Встановлює кількість підлеглих.
+     */
+    public void setTeamSize(int teamSize) {
+        if (teamSize <= 0) {
+            throw new IllegalArgumentException("Кількість підлеглих повинна бути більше 0.");
+        }
+        this.teamSize = teamSize;
+    }
+
+    /**
+     * Повертає рядкове представлення об'єкта.
+     */
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", salary=" + getSalary() +
+                ", teamSize=" + teamSize +
+                '}';
+    }
+}
